e6f383bd388b9a7f16d1c69377a30a345d92633d
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-11-08 19:17:33 +0700

    💄 Tweak navigation bar UIs

--- src/components/layouts/dashboard-layout.tsx
+++ src/components/layouts/dashboard-layout.tsx
@@ -102,7 +102,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <aside className="fixed inset-y-0 left-0 z-10 hidden w-60 flex-col border-r bg-black sm:flex">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-60 flex-col border-r sm:flex">
         <nav className="flex flex-col items-center gap-4 px-2 py-4">
           <div className="flex h-16 shrink-0 items-center px-4">
             <Logo />
@@ -114,9 +114,9 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
               end={item.name !== 'Discussions'}
               className={({ isActive }) =>
                 cn(
-                  'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'text-gray-900 hover:bg-gray-200',
                   'group flex flex-1 w-full items-center rounded-md p-2 text-base font-medium',
-                  isActive && 'bg-gray-900 text-white',
+                  isActive && 'bg-gray-300',
                 )
               }
             >
