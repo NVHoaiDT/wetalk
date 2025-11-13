12c381d6fa95e17034fe2705391dd0022e13fe98
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-11-13 15:57:09 +0700

    💄 Polish auth layout

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -64,7 +64,7 @@ export const AuthLayout = ({ children, title }: LayoutProps) => {
         >
           <div className="w-full p-8">
             <img
-              src="/login-decor-image.png"
+              src="/login-decor-image-1.png"
               alt="Decorative illustration"
               className="rounded-3xl object-cover"
             />
