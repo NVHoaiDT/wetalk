57e40388ed7773f14c11e069a14c544dd211f7e4
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-11-22 14:02:24 +0700

    ✨ Implement authentication for up/down vote post comment

--- src/features/post-comments/components/downvote-post-comment.tsx
+++ src/features/post-comments/components/downvote-post-comment.tsx
@@ -1,10 +1,20 @@
 import { ChevronDown } from 'lucide-react';
+import { Link } from 'react-router';
 
+import { ConfirmationDialog } from '@/components/ui/dialog';
 import { useNotifications } from '@/components/ui/notifications';
+import { paths } from '@/config/paths';
+import { ProtectedAction } from '@/lib/auth';
 
 import { useVotePostComment } from '../api/vote-post-comment';
 
-export const DownVotePostComment = ({ commentId }: { commentId: number }) => {
+type DownVotePostCommentProps = {
+  commentId: number;
+};
+
+export const DownVotePostCommentFallback = ({
+  commentId,
+}: DownVotePostCommentProps) => {
   const { addNotification } = useNotifications();
   const votePostCommentMutation = useVotePostComment({
     mutationConfig: {
@@ -23,5 +33,43 @@ export const DownVotePostComment = ({ commentId }: { commentId: number }) => {
     >
       <ChevronDown className="size-5" />
     </button>
+  );
+};
+
+export const UnauthenticatedFallback = () => {
+  return (
+    <ConfirmationDialog
+      icon="info"
+      title="Join the conversation!"
+      body="Sign up to downvote comments and share your feedback."
+      illustration="https://res.cloudinary.com/djwpst00v/image/upload/v1763792572/vote_d9nmh4.jpg"
+      triggerButton={
+        <button className="rounded text-gray-500 transition-colors hover:bg-blue-50 hover:text-blue-600">
+          <ChevronDown className="size-5" />
+        </button>
+      }
+      confirmButton={
+        <Link
+          to={paths.auth.register.getHref(location.pathname)}
+          replace
+          className="inline-block rounded-md bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700"
+        >
+          Sign up
+        </Link>
+      }
+    ></ConfirmationDialog>
+  );
+};
+
+export const DownVotePostComment = ({
+  commentId,
+}: DownVotePostCommentProps) => {
+  return (
+    <ProtectedAction
+      authenticatedFallback={
+        <DownVotePostCommentFallback commentId={commentId} />
+      }
+      unauthenticatedFallback={<UnauthenticatedFallback />}
+    />
   );
 };
--- src/features/post-comments/components/upvote-post-comment.tsx
+++ src/features/post-comments/components/upvote-post-comment.tsx
@@ -1,10 +1,20 @@
 import { ChevronUp } from 'lucide-react';
+import { Link } from 'react-router';
 
+import { ConfirmationDialog } from '@/components/ui/dialog';
 import { useNotifications } from '@/components/ui/notifications';
+import { paths } from '@/config/paths';
+import { ProtectedAction } from '@/lib/auth';
 
 import { useVotePostComment } from '../api/vote-post-comment';
 
-export const UpVotePostComment = ({ commentId }: { commentId: number }) => {
+type UpVotePostCommentProps = {
+  commentId: number;
+};
+
+export const UpVotePostCommentFallback = ({
+  commentId,
+}: UpVotePostCommentProps) => {
   const { addNotification } = useNotifications();
   const votePostCommentMutation = useVotePostComment({
     mutationConfig: {
@@ -23,5 +33,41 @@ export const UpVotePostComment = ({ commentId }: { commentId: number }) => {
     >
       <ChevronUp className="size-5" />
     </button>
+  );
+};
+
+export const UnauthenticatedFallback = () => {
+  return (
+    <ConfirmationDialog
+      icon="info"
+      title="Join the conversation!"
+      body="Sign up to upvote comments and support helpful contributions."
+      illustration="https://res.cloudinary.com/djwpst00v/image/upload/v1763792572/vote_d9nmh4.jpg"
+      triggerButton={
+        <button className="rounded text-gray-500 transition-colors hover:bg-blue-50 hover:text-blue-600">
+          <ChevronUp className="size-5" />
+        </button>
+      }
+      confirmButton={
+        <Link
+          to={paths.auth.register.getHref(location.pathname)}
+          replace
+          className="inline-block rounded-md bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700"
+        >
+          Sign up
+        </Link>
+      }
+    ></ConfirmationDialog>
+  );
+};
+
+export const UpVotePostComment = ({ commentId }: UpVotePostCommentProps) => {
+  return (
+    <ProtectedAction
+      authenticatedFallback={
+        <UpVotePostCommentFallback commentId={commentId} />
+      }
+      unauthenticatedFallback={<UnauthenticatedFallback />}
+    />
   );
 };
