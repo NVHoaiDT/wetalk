c4c6464a9e7d5daa7ecd37d9a507e75bdd2b8024
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-12-07 14:04:29 +0700

    🎨 Polish the the user profile page and it's query structure

--- src/app/routes/app/profiles/user-profile.tsx
+++ src/app/routes/app/profiles/user-profile.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useParams } from 'react-router';
 
-import { MainErrorFallback } from '@/components/errors/main';
 import { ContentLayout } from '@/components/layouts';
 import { Spinner } from '@/components/ui/spinner';
 import { ProfileContent } from '@/features/profiles/components/profile-content';
@@ -20,9 +19,9 @@ const UserProfileRoute = () => {
   const { data: currentUserData } = useCurrentUser();
   const currentUserId = currentUserData?.data?.id;
 
-  const { data, status } = useUser(Number(userId));
+  const userQuery = useUser(Number(userId));
 
-  if (status === 'pending') {
+  if (userQuery.isPending) {
     return (
       <div className="flex min-h-screen items-center justify-center">
         <Spinner />
@@ -30,14 +29,28 @@ const UserProfileRoute = () => {
     );
   }
 
-  if (status === 'error') {
-    return <MainErrorFallback />;
+  if (userQuery.isError || !userQuery.data?.data) {
+    return (
+      <div className="flex min-h-[400px] w-full items-center justify-center">
+        <div className="flex max-w-full flex-col items-center gap-4 rounded-lg border border-orange-200 bg-white p-8 text-center">
+          <img
+            src="https://res.cloudinary.com/djwpst00v/image/upload/v1763789403/13379593_5219088_iajsfa.svg"
+            alt="question"
+            className="size-96 rounded-full"
+          />
+          <h3 className="text-lg font-semibold text-red-900">
+            Unable to load Profile
+          </h3>
+          <p className="text-sm text-orange-700">
+            This user profile might not exist. Please make sure the URL is
+            correct or try refreshing the page.
+          </p>
+        </div>
+      </div>
+    );
   }
 
-  const user = data?.data;
-  if (!user) {
-    return null;
-  }
+  const user = userQuery.data?.data;
 
   const isOwnProfile = currentUserId === user.id;
 
